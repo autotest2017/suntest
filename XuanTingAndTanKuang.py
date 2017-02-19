3e8f0b94cba02d22afb46b07db4a529f1c40e4a1
wosunzhiping <wosunzhiping@126.com>
2017-02-19 15:16:38 +0800

    自动发送邮件代码

--- XuanTingAndTanKuang.py
+++ XuanTingAndTanKuang.py
@@ -1,22 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+
 
 wd = webdriver.Firefox()
-
 wd.get("http://www.baidu.com")
-
 link = wd.find_element_by_link_text(u"设置")
-
-from selenium.webdriver.common.action_chains import ActionChains
 tmp = ActionChains(wd).move_to_element(link)
-
 tmp.perform()
 
 ActionChains(wd).double_click(link).perform()
-
 ActionChains(wd).click(link).perform()
-
 wd.find_element_by_link_text(u"搜索设置").click()
-
 wd.find_element_by_class_name("prefpanelgo").click()
 
 #wd.switch_to_alert().accept()
